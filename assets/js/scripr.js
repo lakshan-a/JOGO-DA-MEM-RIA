8ce9615a3f884e1320f88ffe0cd4397603bb2fe1
lakshan <lakshanrashmika16302@gmail.com>
2024-05-19 12:52:08 +0530

    updated

--- assets/js/scripr.js
+++ assets/js/scripr.js
@@ -17,3 +17,13 @@ const emojis = [
     "🐮",
   ];
   let openCards = [];
+
+  let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  box.onclick = handleClick;
+  document.querySelector(".game").appendChild(box);
+}
